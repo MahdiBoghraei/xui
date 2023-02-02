22d165abac78b456efbddd24735832a429da528c
MahdiBoghraei <mahdib58006@gmail.com>
2023-02-02 23:17:43 +0330

    f

--- xui.sh
+++ xui.sh
@@ -15,4 +15,4 @@ curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --register-account -m $email
 ~/.acme.sh/acme.sh --issue -d $domain --standalone
 ~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh
