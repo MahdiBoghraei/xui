c04cc8106c7dd72de3b78a5798597819be1ba6aa
MahdiBoghraei <mahdib58006@gmail.com>
2023-02-02 23:15:40 +0330

    f

--- xui.sh
+++ xui.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+while getopts e:d: flag
+do
+    case "${flag}" in
+        e) email=${OPTARG};;
+        d) domain=${OPTARG};;
+    esac
+done
+
 apt update && apt upgrade -y
 apt install curl socat -y
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m $1
-~/.acme.sh/acme.sh --issue -d $2 --standalone
-~/.acme.sh/acme.sh --installcert -d $2 --key-file /root/private.key --fullchain-file /root/cert.crt
-bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh) -y
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
+bash <(curl -Ls https://raw.githubusercontent.com/vaxilu/x-ui/master/install.sh)
