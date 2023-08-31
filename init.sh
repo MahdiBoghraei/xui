99461b21191ce95dd249a2037db77cbf823c84c3
Mahdi Boghraei <88531068+MahdiBoghraei@users.noreply.github.com>
2023-08-31 10:28:07 +0330

    a

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+while getopts e:d: flag
+do
+    case "${flag}" in
+        e) email=${OPTARG};;
+        d) domain=${OPTARG};;
+    esac
+done
+
+apt update && apt upgrade -y
+apt install curl socat -y
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh --register-account -m $email
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.crt
