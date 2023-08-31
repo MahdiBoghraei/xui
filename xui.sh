9ea11199107079c93732996d09e9f59d7936e244
Mahdi Boghraei <88531068+MahdiBoghraei@users.noreply.github.com>
2023-08-31 10:51:21 +0330

    m

--- xui.sh
+++ xui.sh
@@ -10,6 +10,7 @@ done
 
 apt update && apt upgrade -y
 apt install curl socat -y
+apt install cron
 curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 ~/.acme.sh/acme.sh --register-account -m $email
